e4c2bd952ad3108e6a6c31d22c40afb9a51db521
unknown <yang.yang6@YTX.COM>
2019-10-25 18:38:47 +0800

    优化选项卡面向对象写法

--- kkb-js/17-面向对象/选项卡/index.js
+++ kkb-js/17-面向对象/选项卡/index.js
@@ -3,24 +3,45 @@
 {
   class Tab {
     constructor(el) {
-      this.btns = document.querySelector(el).querySelectorAll('.btn');
-      this.views = document.querySelector(el).querySelectorAll('.view');
+      this.el = document.querySelector(el)
+      this.btns = this.el.querySelectorAll('.btn');
+      this.views = this.el.querySelectorAll('.view');
+      this.currentIndex = 0;
       this.prevIndex = 0;
       this.init();
     }
     init() {
       Array.from(this.btns).forEach((item, index) => {
         item.addEventListener('click', () => {
-          this.btns[index].classList.add('active');
-          this.btns[this.prevIndex].classList.remove('active');
-          this.views[index].classList.add('show');
-          this.views[this.prevIndex].classList.remove('show');
+          this.currentIndex = index;
+          this.tab();
           this.prevIndex = index;
         });
       });
     }
+    tab() {
+      this.btns[this.currentIndex].classList.add('active');
+      this.btns[this.prevIndex].classList.remove('active');
+      this.views[this.currentIndex].classList.add('show');
+      this.views[this.prevIndex].classList.remove('show');
+    }
+  }
+
+  class AutoTab extends Tab {
+    constructor(el) {
+      super(el);
+    }
+    autoplay() {
+      setInterval(() => {
+        this.currentIndex += 1;
+        this.currentIndex = this.currentIndex % this.btns.length;
+        this.tab();
+        this.prevIndex = this.currentIndex;
+      }, 1000);
+    }
   }
 
   const tab1 = new Tab('.container1');
-  const tab2 = new Tab('.container2');
+  const tab2 = new AutoTab('.container2');
+  tab2.autoplay();
 }
