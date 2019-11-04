572041f8a9fa7b0c5553b65c15b99c8a2c540302
unknown <yang.yang6@YTX.COM>
2019-11-04 18:34:13 +0800

    学习proxy

--- kkb-js/18-ES6高阶使用/MVVM基本原理/myVue.js
+++ kkb-js/18-ES6高阶使用/MVVM基本原理/myVue.js
@@ -1,5 +1,61 @@
-const str = '{{ message }}';
-const reg = /\{\{\s*(\S+)\s*\}\}/g;
-if (reg.test(str)) {
-  console.log(RegExp.$1);
+class MyVue extends EventTarget {
+  constructor(options) {
+    super();
+    this.options = options;
+    this.compile();
+    this.observers(this.options.data);
+  }
+  observers(data) {
+    Object.keys(data).forEach(key => {
+      this.handleData(data, key, data[key]);
+    });
+  }
+  handleData(data, key, value) {
+    const _this = this;
+    Object.defineProperty(data, key, {
+      enumerable: true,
+      configurable: true,
+      get() {
+        return value;
+      },
+      set(newValue) {
+        const event = new CustomEvent(key, {
+          detail: newValue
+        });
+        _this.dispatchEvent(event);
+        newValue = value;
+      }
+    });
+  }
+  compile() {
+    const el = document.querySelector(this.options.el);
+    const childNodes = el.childNodes;
+    this.handleNodes(childNodes);
+  }
+  handleNodes(childNodes) {
+    childNodes.forEach(node => {
+      switch (node.nodeType) {
+        // 节点
+        case 1:
+          this.handleNodes(node.childNodes);
+          break;
+          
+        // 文本
+        case 3:
+          const reg = /\{\{\s*(\S+)\s*\}\}/;
+          const text = node.textContent;
+          if (reg.test(text)) {
+            const $1 = RegExp.$1;
+            node.textContent = text.replace(reg, this.options.data[$1]);
+            this.addEventListener($1, e => {
+              const oldValue = node.textContent;
+              const newValue = e.detail;
+              const reg = new RegExp(oldValue, 'g');
+              node.textContent = node.textContent.replace(reg, newValue);
+            });
+          }
+          break;
+      }
+    });
+  }
 }
--- kkb-js/18-ES6高阶使用/test.js
+++
@@ -1,3 +0,0 @@
-const reg = /\{\{(.*)\}\}/;
-const str = '{{ aaa }}'.replace(/\s*/g, '');
-console.log(str.match(reg)[1]);
