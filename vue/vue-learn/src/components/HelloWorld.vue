5edc9b3098e71e92f0b157f108e47b75ffa9a42c
unknown <yang.yang6@YTX.COM>
2019-11-07 18:26:21 +0800

    学习sass

---
+++ vue/vue-learn/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div>{{ html }}</div>
+    <div v-html="html"></div>
+    <div v-text="html"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      html: '<span style="color: red;">Hello World</span>'
+    };
+  }
+};
+</script>
