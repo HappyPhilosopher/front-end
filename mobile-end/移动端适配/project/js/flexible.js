ea5b5f0a7a52917a36ea84a7d13d627dd5e6a1bc
Yang <576772193@qq.com>
2019-12-10 23:43:36 +0800

    完成移动端header布局

--- mobile-end/移动端适配/project/js/flexible.js
+++ mobile-end/移动端适配/project/js/flexible.js
@@ -2,7 +2,7 @@
   'use strict';
   var docEl = document.documentElement;
   var viewportEl = document.querySelector('meta[name="viewport"]');
-  var maxWidth= 540;
+  var maxWidth = 540;
   var minWidth = 320;
   var dpr = window.devicePixelRatio;
   dpr = dpr >= 3 ? 3 : dpr >= 2 ? 2 : 1;
@@ -15,7 +15,7 @@
     ', minimum-scale=' +
     scale +
     ', user-scalable=no';
-  
+
   docEl.setAttribute('data-dpr', dpr);
   docEl.setAttribute('max-width', maxWidth);
   docEl.setAttribute('min-width', minWidth);
@@ -29,9 +29,22 @@
     document.head.appendChild(viewportEl);
   }
 
+  setRemUnit();
+
+  window.addEventListener('resize', setRemUnit);
+
   function setRemUnit() {
     // ratio = 750 / 40;
     // 750px 为设计稿宽度，40px 为根元素字体大小
     var ratio = 18.75;
+    var viewWidth = window.innerWidth;
+
+    if (maxWidth && viewWidth / dpr > maxWidth) {
+      viewWidth = maxWidth * dpr;
+    } else if (minWidth && viewWidth / dpr < minWidth) {
+      viewWidth = minWidth * dpr;
+    }
+
+    docEl.style.fontSize = viewWidth / ratio + 'px';
   }
 })();
